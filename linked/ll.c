34d9723402e93705bdd9f86699535b78fa45cadf
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-03-26 00:32:18 -0500

    all required functions made for ll

--- linked/ll.c
+++ linked/ll.c
@@ -5,108 +5,184 @@
 
 typedef struct Node {
 	char content[256];
-	struct Node *next;   
-	struct Node *prev;   
+	struct Node *next;
+	struct Node *prev;
 } Node;
 
-Node newNode(char content[]) {
-	printf("%s\n", "newNode()");
-	struct Node new;
+Node* newNode(char content[]) {
+	// printf("%s\n", "newNode()");
+	Node* new = (Node*)malloc(sizeof(Node));
 	
-	strcpy(new.content, content);
-	new.next = NULL;
-	new.prev = NULL;
+	strcpy(new->content, content);
+	new->next = NULL;
+	new->prev = NULL;
 	return new;
 }
 
-void appendNode(Node prev, Node new) {
-	printf("%s\n", "appendNode()");
-	prev.next = &new;
-	new.prev = &prev;
+void insertNode(Node *prev, Node *new, Node *next) {
+	// printf("%s\n", "insertNode()");
+	prev->next = new;
+	new->prev = prev;
+	if (next != NULL) {
+		new->next = next;
+		next->prev = new;
+	}
 }
 
-int size(Node head) {
-	printf("%s\n", "size()");
-	Node next = head;
-	int i = 0;
-	while (next.next != NULL) {
-		next = *next.next;
+void appendNode(Node *head, Node *new) {
+	// printf("%s\n", "appendNode()");
+	Node *next = head;
+	while (next->next != NULL) {
+		next = next->next;
+	}
+	insertNode(next, new, NULL);
+}
+
+void prependNode(Node *head, Node *new) {
+	// printf("%s\n", "prependNode()");
+	Node *first = head;
+	while (first->prev != NULL) {
+		first = first->prev;
+	}
+	new->next = first;
+	first->prev = new;
+}
+
+Node* head(Node *node) {
+	// printf("%s\n", "head()");
+	Node *prev = node;
+	while (prev->prev != NULL) {
+		prev = prev->prev;
+	}
+	return prev;
+}
+
+int size(Node *node) {
+	// printf("%s\n", "size()");
+	Node *next = head(node);
+	int i = 1;
+	while (next->next != NULL) {
+		next = next->next;
 		i++;
 	}
 	return i;
 }
 
-Node head(Node node) {
-	printf("%s\n", "head()");
-	Node prev = node;
-	while (prev.prev != NULL) {
-		prev = *prev.prev;
-	}
-	return prev;
-}
-
-Node tail(Node node) {
-	printf("%s\n", "tail()");
-	Node next = node;
-	while (next.next != NULL) {
-		next = *next.next;
+Node* tail(Node *node) {
+	// printf("%s\n", "tail()");
+	Node *next = node;
+	while (next->next != NULL) {
+		next = next->next;
 	}
 	return next;
 }
 
-Node at(int index, Node head) {
-	printf("%s\n", "at()");
+Node* at(int index, Node *node) {
+	// printf("%s\n", "at()");
 	int i = 0;
-	Node current = head;
-	while (&current != NULL && i < index) {
-		current = *current.next;
+	Node *current = head(node);
+	while (current != NULL && i < index) {
+		current = current->next;
 		i++;
 	}
 	return current;
 }
 
-const char * pop(char* output, Node head){
-	printf("%s\n", "pop()");
-	Node newHead = *head.next;
-	strcpy(output, head.content);
-	newHead.prev = NULL;
-	// NOTE: Look into proper memory management
+const char * pop(char* output, Node *node){
+	// printf("%s\n", "pop()");
+	Node *beginning = head(node);
+	Node *newHead = beginning->next;
+	strcpy(output, beginning->content);
+	newHead->prev = NULL;
+	free(beginning);
 	return output;
 }
 
-int findIndex(Node head, char value[]) {
-	printf("%s\n", "findIndex()");
-	int index = -1;
-	Node next = head;
-	int count = 0;
-	while (&next != NULL) {
-		if (next.content == value) {
-			return index;
+bool contains(Node *head, char value[]) {
+	// printf("%s\n", "contains()");
+	Node *next = head;
+	while (next != NULL) {
+		if (strcmp(next->content, value) == 0) {
+			return true;
 		}
+		next = next->next;
 	}
-	return index;
+	return false;
 }
 
-char *toString(Node head) {
-	printf("%s\n", "toString()");
-	int len = size(head);
-	Node next = head;
-	char *buffer = "NULL -> ";
-	while (next.next != NULL) {
-		asprintf(&buffer, "%s(%s) -> ", buffer, next.content);
-		next = *next.next;
+int findIndex(Node *node, char value[]) {
+	// printf("%s\n", "findIndex()");
+	int index = 0;
+	Node *next = head(node);
+	while (next != NULL) {
+		if (strcmp(next->content, value) == 0) {
+			return index;
+		}
+		next = next->next;
+		index++;
 	}
-	asprintf(&buffer, "%s(%s) -> NULL", buffer, next.content);
+
+	return -1;
+}
+
+char *toString(Node *node) {
+	// printf("%s\n", "toString()");
+	Node *beginning = head(node);
+	int len = size(beginning);
+	Node *next = beginning;
+	char *buffer = "";
+	while (next->next != NULL) {
+		asprintf(&buffer, "%s(%s) -> ", buffer, next->content);
+		next = next->next;
+	}
+	asprintf(&buffer, "%s(%s) -> NULL", buffer, next->content);
 	return buffer;
 }
 
 int main(int argc, char *argv[])
 {
-	Node head = newNode("A");
-	printf("%s\n", toString(head));
-
-	appendNode(head, newNode("B"));
-	printf("%s\n", toString(head));
+	printf("=== LinkedList Demo ===\n\n");
+	
+	printf("1. Creating list and appending values:\n");
+	Node *list = newNode("First");
+	appendNode(list, newNode("Second"));
+	appendNode(list, newNode("Third"));
+	printf("List: %s\n", toString(list));
+	printf("Size: %d\n\n", size(list));
+	
+	printf("2. Prepending a value:\n");
+	prependNode(list, newNode("Zero"));
+	printf("List after prepend: %s\n", toString(list));
+	printf("Size: %d\n\n", size(list));
+	
+	printf("3. Getting head and tail:\n");
+	printf("Head: %s\n", head(list)->content);
+	printf("Tail: %s\n", tail(list)->content);
+	printf("Size: %d\n\n", size(list));
+	
+	printf("4. Accessing element at index:\n");
+	printf("Element at index 0: %s\n", at(0, list)->content);
+	printf("Element at index 2: %s\n", at(2, list)->content);
+	printf("Element at index 3: %s\n\n", at(3, list)->content);
+	
+	printf("5. Checking contains():\n");
+	printf("Contains 'Second': %s\n", contains(list, "Second") ? "true" : "false");
+	printf("Contains 'Nonexistent': %s\n\n", contains(list, "Nonexistent") ? "true" : "false");
+	
+	printf("6. Finding index of value:\n");
+	printf("Index of 'First': %d\n", findIndex(list, "First"));
+	printf("Index of 'Third': %d\n", findIndex(list, "Third"));
+	printf("Index of 'Missing': %d\n\n", findIndex(list, "Missing"));
+	
+	printf("7. Popping head:\n");
+	char popped[256];
+	pop(popped, list);
+	list = list->next;
+	printf("Popped: %s\n", popped);
+	printf("List after pop: %s\n", toString(list));
+	printf("New size: %d\n\n", size(list));
 
 	return EXIT_SUCCESS;
 }
+
+
